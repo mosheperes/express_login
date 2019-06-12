a4d5c57c2172a622fd13d65c090058ee970a5e9e
Moshe Peres <mosheper99@gmail.com>
2019-06-12 13:43:10 -0400

    final

--- routes/index.js
+++ routes/index.js
@@ -12,4 +12,21 @@ router.get('/word',(req, res)=> {
   res.send({word: word});
 });
 
+router.get('/words/:count',(req, res)=> {
+  let count = Number(req.params.count);
+  let words = randomwords({exactly: count, join: ' '});
+  res.send({word: words});
+});
+
+router.get('/sentence', (req,res)=> {
+  min= (isNaN(Number(req.query.min))) ? 1 : Number(req.query.min);
+  max= (isNaN(Number(req.query.max))) ? min + 1: Number(req.query.max);
+  let sentence = randomwords({min: min, max: max, join: ' '});
+  res.send({sentence: sentence});
+});
+
+router.post('/word',(req, res)=>{
+  res.send({word: 'Funny'});
+});
+
 module.exports = router;
