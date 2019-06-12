e7acd23b69eaf818aa05d60054b7fa25782b2da9
Moshe Peres <mosheper99@gmail.com>
2019-06-12 11:52:28 -0400

    added some api requests

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
+let randomwords = require('random-words');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/word',(req, res)=> {
+  let word = randomwords(1);
+  res.send({word: word});
 });
 
 module.exports = router;
